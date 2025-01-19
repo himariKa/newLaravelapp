37f1dbdea39de48e31c931768d99daebc0261d4d
鰈﨑ひまり <k.himari.1349@gmail.com>
2025-01-19 22:16:42 +0900

    リクエストとレスポンスの内容を画面に表示

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ EOF;
 // Route::get('hello','App\Http\Controllers\HelloController@index');
 // Route::get('hello/other','App\Http\Controllers\HelloController@other');
 
-Route::get('hello','App\Http\Controllers\HelloController');
+Route::get('hello','App\Http\Controllers\HelloController@index');
 
 
 require __DIR__.'/auth.php';
