6b64ec639fdaa233ada5761cd61c50c06ee54fe5
鰈﨑ひまり <k.himari.1349@gmail.com>
2025-01-25 21:35:30 +0900

    クエリー文字列を使ってIDを表示される

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ EOF;
 // Route::get('hello','App\Http\Controllers\HelloController@index');
 // Route::get('hello/other','App\Http\Controllers\HelloController@other');
 
-Route::get('hello/{id?}','App\Http\Controllers\HelloController@index');
+Route::get('hello','App\Http\Controllers\HelloController@index');
 // Route::get('hello',function(){
 //     return view('hello.index');
 // });
